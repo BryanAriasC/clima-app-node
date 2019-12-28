f30b17c51b1fed865b5fb38cf6f086debc461b26
David <bariasinv@gmail.com>
2019-12-28 13:11:37 -0500

    Primer Comit

---
+++ lugar/lugar.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+
+const getLugarLatLng = async(dir) => {
+
+
+    const encodeUrl = encodeURI(dir);
+
+    const instance = axios.create({
+        baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${ encodeUrl }`,
+        headers: {
+            'x-rapidapi-host': 'devru-latitude-longitude-find-v1.p.rapidapi.com',
+            'x-rapidapi-key': '377fd8f5ccmshbf4e80b8d08df60p1b6757jsn3985d6eab884'
+        }
+    });
+
+    const resp = await instance.get()
+
+    if (resp.data.Results.length == 0) {
+        throw new Error(`No hay resultados para ${ dir }`);
+    }
+
+    const data = resp.data.Results[0];
+    const direccion = data.name;
+    const lat = data.lat;
+    const lng = data.lon;
+
+    return {
+        direccion,
+        lat,
+        lng
+    }
+
+}
+
+module.exports = {
+    getLugarLatLng
+}
